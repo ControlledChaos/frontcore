d978088db4ee6e2cae4b1634c2243010b00a11e9
ControlledChaos <greg@ccdzine.com>
2021-07-31 10:56:42 -0700

    Enqueue RTL blocks.

--- includes/classes/frontend/class-assets.php
+++ includes/classes/frontend/class-assets.php
@@ -97,6 +97,10 @@ class Assets {
 		if ( function_exists( 'has_blocks' ) ) {
 			if ( has_blocks() ) {
 				wp_enqueue_style( 'fct-blocks', get_theme_file_uri( '/assets/css/blocks' . $this->suffix() . '.css' ), [ 'fct-theme' ], FCT_VERSION, 'all' );
+
+				if ( is_rtl() ) {
+					wp_enqueue_style( 'fct-blocks-rtl', get_theme_file_uri( '/assets/css/blocks-rtl' . $this->suffix() . '.css' ), [ 'fct-theme' ], FCT_VERSION, 'all' );
+				}
 			}
 		}
 
