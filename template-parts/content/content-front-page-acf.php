86b9914d03fe3537c60f8855ff4d7c9df39a04bb
ControlledChaos <greg@ccdzine.com>
2021-08-24 10:16:27 -0700

    Front page heading element.

--- template-parts/content/content-front-page-acf.php
+++ template-parts/content/content-front-page-acf.php
@@ -19,7 +19,9 @@ use FrontCore\Classes\Front as Front;
 	<header class="entry-header">
 		<?php
 
-		if ( is_singular() ) :
+		if ( is_front_page() ) :
+			the_title( '<h2 class="entry-title">', '</h2>' );
+		elseif ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
