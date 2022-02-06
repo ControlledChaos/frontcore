aab86461231f34a542948b29835a91a064746b22
ControlledChaos <greg@ccdzine.com>
2022-02-06 13:59:44 -0800

    Change menu toggle class.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -31,12 +31,12 @@
 	}
 
 	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
+		if ( -1 !== container.className.indexOf( 'toggled-open' ) ) {
+			container.className = container.className.replace( ' toggled-open', '' );
 			button.setAttribute( 'aria-expanded', 'false' );
 			menu.setAttribute( 'aria-expanded', 'false' );
 		} else {
-			container.className += ' toggled';
+			container.className += ' toggled-open';
 			button.setAttribute( 'aria-expanded', 'true' );
 			menu.setAttribute( 'aria-expanded', 'true' );
 		}
