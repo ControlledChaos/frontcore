3f3fd7079c2d1d18c80fe2a0dd14b7d9ee532a70
ControlledChaos <greg@ccdzine.com>
2022-09-22 09:47:09 -0700

    Remover incorrect esc_attr function.

--- templates/template-parts/footer/footer-default.php
+++ templates/template-parts/footer/footer-default.php
@@ -18,13 +18,13 @@ $copyright = sprintf(
 	'<p class="copyright-text" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">&copy; <span class="screen-reader-text">%1s</span><span itemprop="copyrightYear">%2s</span> <span itemprop="copyrightHolder">%3s.</span> %4s.</p>',
 	esc_html__( 'Copyright ', 'frontcore' ),
 	date( 'Y' ),
-	esc_attr( get_bloginfo( 'name' ) ),
+	get_bloginfo( 'name' ),
 	esc_html__( 'All rights reserved', 'frontcore' )
 );
 
 ?>
 <footer id="colophon" class="site-footer">
 	<div class="footer-content global-wrapper footer-wrapper">
-			<?php echo apply_filters( 'fct_footer_copyright', $copyright ); ?>
+		<?php echo apply_filters( 'fct_footer_copyright', $copyright ); ?>
 	</div>
 </footer>
