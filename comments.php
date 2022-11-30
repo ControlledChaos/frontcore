00ef4650728aa771143c709c3d51f391798a9bb5
ControlledChaos <greg@ccdzine.com>
2022-11-30 11:22:39 -0800

    Docblock edit, remove namespace.

--- comments.php
+++ comments.php
@@ -2,20 +2,13 @@
 /**
  * Comments template
  *
- * This is the template that displays the area of the page that
- * contains both the current comments and the comment form.
+ * This file is provided to prevent deprecation warnings
+ * if not included in the theme.
+ *
+ * @see includes/users/comments.php for comment template filter.
  *
  * @package    Front_Core
  * @subpackage Templates
  * @category   Users
  * @since      1.0.0
- */
-
-namespace FrontCore;
-
-/**
- * This file is provided to prevent deprecation warnings
- * if not included in the theme.
- *
- * @see includes/users/comments.php for comment template filter.
  */
