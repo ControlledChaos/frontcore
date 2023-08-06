4a2ac3cb90487018d5e23e59f667483b423f72ce
ControlledChaos <greg@ccdzine.com>
2023-08-06 11:37:24 -0700

    Added sample shortcode from companion plugin to filtered content.

--- templates/template-parts/content/content-single-sample.php
+++ templates/template-parts/content/content-single-sample.php
@@ -11,6 +11,7 @@
  * @since      1.0.0
  */
 
+// Get post type name.
 $object = get_post_type_object( get_post_type( get_the_ID() ) );
 
 if ( $object->labels->singular_name ) {
@@ -18,6 +19,17 @@ if ( $object->labels->singular_name ) {
 } else {
 	$name = $object->labels->name;
 }
+
+/**
+ * Sample shortcode is from this themes companion
+ * plugin, used for demonstration.
+ *
+ * Sample shortcode text.
+ */
+$code_text = __( 'This is a sample shortcode, using the <code>[sample]</code> tag, appended to the filtered content using the <code>do_shortcode()</code> function.' );
+
+// Sample shortcode tag.
+$code_tag = "[sample wrap_text='yes' text_class='sample-paragraph' text_weight='600']{$code_text}[/sample]";
 
 printf(
 	__( '<p>Filtered content for %s #%s</p>', 'sitecore' ),
@@ -33,3 +45,8 @@ printf(
 	__( '<p>The template for this notice is in the %s theme.</p>', 'sitecore' ),
 	wp_get_theme()
 );
+
+// Append content with shortcode.
+if ( shortcode_exists( 'sample' ) ) {
+	echo do_shortcode( $code_tag );
+}
